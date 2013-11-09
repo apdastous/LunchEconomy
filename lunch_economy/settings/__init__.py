479ad08dec0ff5db1166607f20ee58571f98e7a0
apdastous <apdastous@gmail.com>
2013-11-09 13:28:21 -0600

    Settings init now looks at user to determine which settings to import

--- lunch_economy/settings/__init__.py
+++ lunch_economy/settings/__init__.py
@@ -1,1 +1,9 @@
-from dev import *
+from getpass import getuser
+
+user = getuser()
+if user == 'jenkins':
+    from jenkins import *
+elif user == 'www-data':
+    from base import *
+else:
+    from dev import *
