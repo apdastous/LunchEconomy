d98307587d9184434ca0ec56b239fbdfa236b0ac
apdastous <apdastous@gmail.com>
2013-11-09 13:42:32 -0600

    Would probably help if I used the right path

--- build/jenkins.sh
+++ build/jenkins.sh
@@ -6,4 +6,4 @@ source env/bin/activate
 
 pip install -r requirements/jenkins.txt
 
-python manage.py jenkins --coverage-exclude=COVERAGE_EXCLUDES_FOLDERS  --settings=settings.jenkins
+python manage.py jenkins --coverage-exclude=COVERAGE_EXCLUDES_FOLDERS  --settings=lunch_economy.settings.jenkins
